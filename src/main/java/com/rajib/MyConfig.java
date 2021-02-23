5858eab68480755f9f01b06e6548660798ed74c7
Rajib Sarker <rajibaustcse@gmail.com>
2021-02-23 12:50:34 +0600

    project rename
    minor changes

--- src/main/java/com/rajib/MyConfig.java
+++ src/main/java/com/rajib/MyConfig.java
@@ -6,7 +6,6 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
-
 @Configuration
 @EnableWebMvc
 @ComponentScan("com.rajib")
